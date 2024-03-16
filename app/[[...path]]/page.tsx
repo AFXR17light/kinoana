c2131e1af5b367c7333f0197ae5ffbf8f33a286f
AFXR Hikari <theaurora@outlook.com>
2024-03-16 15:34:49 +0800

    fix fs async issue

--- app/[[...path]]/page.tsx
+++ app/[[...path]]/page.tsx
@@ -1,11 +1,11 @@
-import fs from 'fs';
+import { promises as fs } from 'fs';
 import path from 'path';
 import { notFound } from "next/navigation";
 import { compileMDX } from 'next-mdx-remote/rsc';
 
 import Layout from '../layout';
 
-const contentDir = 'content';
+const contentDir = '\\content';
 
 interface FileOrDirectory {
   type: string;
@@ -14,27 +14,35 @@ interface FileOrDirectory {
   content?: string;
 }
 
-function fileSource() {
+async function fileSource() {
   let filesAndDirectories: FileOrDirectory[] = [];
-  const getFile = (filePath: string) => {
-    if (fs.statSync(filePath).isDirectory()) {
-      filesAndDirectories.push({ type: 'directory', path: filePath.replace(contentDir + path.sep, '') });
-      fs.readdirSync(filePath).forEach(fileName => getFile(path.join(filePath, fileName)));
+  const getFile = async (filePath: string) => {
+    // console.log('curent path: ', filePath);
+    let currentFireOrDir: FileOrDirectory = { type: '', path: '' };
+    if ((await fs.stat(filePath)).isDirectory()) {
+      currentFireOrDir = { type: 'directory', path: filePath };
+      const files = await fs.readdir(filePath);
+      await Promise.all(files.map(fileName => getFile(path.join(filePath, fileName))));
     } else {
       const filePathObj = path.parse(filePath);
-      const strippedFilePath = path.join(filePathObj.dir, filePathObj.name).replace(contentDir + path.sep, '');
-      const fileContent = fs.readFileSync(filePath, 'utf8');
-      filesAndDirectories.push({ type: 'file', path: strippedFilePath, extension: filePathObj.ext, content: fileContent });
+      const strippedFilePath = path.join(filePathObj.dir, filePathObj.name);
+      const fileContent = await fs.readFile(filePath, 'utf8');
+      currentFireOrDir = { type: 'file', path: strippedFilePath, extension: filePathObj.ext, content: fileContent };
     }
+    // console.log('------------------ current fod path: ', currentFireOrDir.path);
+    filesAndDirectories.push(currentFireOrDir);
   }
-  getFile(contentDir);
+  await getFile(process.cwd() + contentDir);
+  // console.log(filesAndDirectories);
   return filesAndDirectories;
 }
 
 export default async function Page({ params }: { params: { path: string[] } }) {
   const { path } = params;
-  let filesAndDirectories = fileSource();
-  let fileOrDir = filesAndDirectories.find((fd: FileOrDirectory) => fd.path == (path ? path.join('\\') : 'index'));
+  const filesAndDirectories = await fileSource();
+  // console.log(filesAndDirectories[2].path?.replace(process.cwd() + contentDir, ''));
+  // console.log('path: ', '\\' + path.join('\\'));
+  let fileOrDir = filesAndDirectories.find((fd: FileOrDirectory) => fd.path?.replace(process.cwd() + contentDir, '') == (path ? '\\' + path.join('\\') : '\\index'));
   if (!fileOrDir) notFound();
   const { content, frontmatter } = await compileMDX<{ title: string }>({
     source: fileOrDir.content || '',
@@ -42,18 +50,18 @@ export default async function Page({ params }: { params: { path: string[] } }) {
   })
   return (
     <Layout
-    // props={{
-    //   pathFragments: path ? path : [],
-    //   fileOrDir: fileOrDir,
-    //   title: frontmatter?.title,
-    // }}
-    >123123
-      {/* {fileOrDir.content && (fileOrDir.extension === '.mdx' || fileOrDir.extension === '.md') && (
+      props={{
+        pathFragments: path ? path : [],
+        fileOrDir: fileOrDir,
+        title: frontmatter?.title,
+      }}
+    >
+      {fileOrDir.content && (fileOrDir.extension === '.mdx' || fileOrDir.extension === '.md') && (
         <div>
           <div>* {fileOrDir.type}: &quot;{fileOrDir.path}{fileOrDir.extension}&quot;</div>
           {content}
         </div>
-      )} */}
+      )}
     </Layout>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,35 +8,35 @@ export const metadata: Metadata = {
 };
 
 export default function Layout({
-  children,
+  children, props,
 }: Readonly<{
   children: React.ReactNode;
-  // props: {
-  //   pathFragments: string[];
-  //   fileOrDir: {
-  //     type: string;
-  //     path?: string;
-  //     extension?: string;
-  //     content?: string;
-  //   };
-  //   title?: string;
-  // };
+  props: {
+    pathFragments: string[];
+    fileOrDir: {
+      type: string;
+      path?: string;
+      extension?: string;
+      content?: string;
+    };
+    title?: string;
+  };
 }>) {
-  // const { pathFragments, fileOrDir, title } = { ...props };
+  const { pathFragments, fileOrDir, title } = { ...props };
   return (
     <html lang="en">
       <body>
-        {/* {pathFragments?.length !== 0 ? 
-        (pathFragments?.map((fragment: string, index: number) => {
-          let cumulativePath = '';
-          for (let i = 0; i <= index; i++) {
-            cumulativePath += '/' + pathFragments[i];
-          }
-          return <Link style={{ fontSize: '2em', fontWeight: 'bold' }} key={fragment} href={cumulativePath}>/{fragment}</Link>
-        }))
-        : (<Link style={{ fontSize: '2em', fontWeight: 'bold' }} href='/'>/</Link>)}
+        {pathFragments?.length !== 0 ?
+          (pathFragments?.map((fragment: string, index: number) => {
+            let cumulativePath = '';
+            for (let i = 0; i <= index; i++) {
+              cumulativePath += '/' + pathFragments[i];
+            }
+            return <Link style={{ fontSize: '2em', fontWeight: 'bold' }} key={fragment} href={cumulativePath}>/{fragment}</Link>
+          }))
+          : (<Link style={{ fontSize: '2em', fontWeight: 'bold' }} href='/'>/</Link>)}
         <h1>{title}</h1>
-        {fileOrDir && <hr />} */}
+        {fileOrDir && <hr />}
         {children}
       </body>
     </html>
